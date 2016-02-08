b3f955026223023ae4288f00b3b676f215693969
Adam Shuman <shumanac@montreat.edu>
2016-02-08 14:21:00 -0500

    Update clamavinstaller.sh

--- clamavinstaller.sh
+++ clamavinstaller.sh
@@ -15,6 +15,9 @@ read -p "Do you want to download clamav? [y/n] " answerDownloadClamav
 read -p "Would you like to configure and install clamav? [y/n] " answerInstallClamav
 read -p "would you like to run a scan? [y/n] " answerRunScan 
 }
+
+questions
+
 if [[ $answerDownloadClamav = y ]] ; then
 	cd /home
 	wget https://www.clamav.net/downloads/production/clamav-0.97.3.tar.gz --no-check-certificate
