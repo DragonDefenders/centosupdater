9c33b9ffb4dfbcc4a4e712cd429bd5fd2fc8c89c
Adam Shuman <shumanac@montreat.edu>
2016-02-10 20:36:55 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@ printf "
 function questions() {
 read -p "Would you like to install yumUpdater? [y/n] " answerInstallYumUpdater
 read -p "Do you want to download clamavinstaller? [y/n] " answerInstallClamavInstaller
-read -p "Would you like to update path? [y/n] " answerUpdate
+read -p "Would you like to update path? [y/n] " answerUpdatePath
 }
 questions
 
@@ -24,6 +24,9 @@ if [[ $answerInstallYumUpdater = y ]] ; then
 	wget https://raw.githubusercontent.com/DragonDefenders/centosupdater/master/yumUpdater.sh --no-check-certificate -O /root/yumUpdater.sh
 fi
 
+if [[ $answerUpdatePath = y ]] ; then
+	PATH=$PATH /sbin
+fi
 
 function pause () {
         read -p "$*"
