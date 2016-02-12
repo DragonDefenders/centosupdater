7e7b5ae8dbbca92eb3a8a3e36dca35a8fb221803
Adam Shuman <shumanac@montreat.edu>
2016-02-12 17:49:39 -0500

    Create tripwireInstaller.sh

---
+++ tripwireInstaller.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+printf "        
+                  #############################
+                  # CentOS Tripwire Installer #
+                  #############################
+                #################################
+                #This script MUST be run as root#
+                #################################
+   ##############################################################
+   # Welcome, This script will install scipts for you and stuff #
+   ##############################################################\n\n"
+function questions() {
+read -p "Would you like to install tripwire? [y/n] " answerInstallTripwire
+read -p "Would you like to run tripwire? [y/n] " answerRunTripwire
+}
+questions
+
+if [[ $answerInstallTripwire = y ]] ; then
+  wget ftp://ftp.pangeia.com.br/pub/seg/pac/chkrootkit.tar.gz -O /usr/local/src/chkrootkit.tar.gz
+  tar xvzf chkrootkit.tar.gz
+  cd /usr/local/src/chkrootkit-*
+  make sense
+fi
+
+if [[ $answerRunTripwire = y ]] ; then
+  cd /usr/local/src/chkrootkit-*
+  ./chkrootkit
+fi
+
+function pause () {
+        read -p "$*"
+}
+
+pause '
+press [Enter] key to exit...
+'
